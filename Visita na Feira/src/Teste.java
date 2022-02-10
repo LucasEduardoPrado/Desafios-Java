518b9093f35ff58494e88a0c7b14cb2cc4eaa5ce
LucasEduardoPrado <lukasedu635@gmail.com>
2022-02-09 21:22:23 -0300

    Visita na Feira,  O Filme, Área do Círculo, Média 1, Exibindo Números Pares, Dama

---
+++ Visita na Feira/src/Teste.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner leitor = new Scanner(System.in);
+
+
+        int a = leitor.nextInt();
+        int b = leitor.nextInt();
+
+        int total = a + b;
+
+        System.out.println("X = " + total);
+
+    }
+}
